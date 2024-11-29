e03c5447f8516a8b80728b120d3dc020cefbe510
Bismillah <bismillah@localhost.localdomain>
2024-11-29 11:31:34 +0800

    trigger

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ type ApiResponse struct {
 	Code    int         `json:"code"`
 	Status  string      `json:"status"`
 	Message string      `json:"message,omitempty"`
-
 	Data    interface{} `json:"data,omitempty"`
 }
 
